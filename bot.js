cf479e3ad772b6ca863f8d7f3566a144902b2b37
eddichen <echen@inviqa.com>
2018-03-12 08:45:55 +0000

    adding andy's starter

--- bot.js
+++ bot.js
@@ -1,11 +1,86 @@
-var dirs = 'nesw';
+"use strict";
+
+function splitString (string, size) {
+	var re = new RegExp('.{1,' + size + '}', 'g')
+	return string.match(re)
+}
+
+class Game {
+    constructor(game)
+    {
+        this.board = new Board(game.board)
+        this.game = game
+        this.heros = game.heroes
+    }
+
+    atPositionRelativeToHero(hero, pos) {
+        const absolutePos = {x: hero.pos.x + pos.x, y: hero.pos.y + pos.y}
+
+        return this.board.atPosition(absolutePos)
+    }
+}
+
+class Board {
+    constructor(board)
+    {
+        this.board = splitString(board.tiles, board.size * TILE_WIDTH).map(row => splitString(row, TILE_WIDTH))
+    }
+
+    atPosition(pos)
+    {
+        if (pos.x < 0 || pos.x >= this.board.size || pos.y < 0 || pos.y >= this.board.size) {
+            return WALL
+        }
+        return this.board[pos.x][pos.y]
+    }
+}
+
+const TILE_WIDTH = 2
+
+const WOOD = '#'
+const HERO = '@'
+const TAVERN = '['
+const MINE = '$'
+const AIR = ' '
+const WALL = '|'
+
+const NORTH = {x: -1, y: 0, action: 'North'}
+const EAST = {x: 0, y: 1, action: 'East'}
+const SOUTH = {x: 1, y: 0, action: 'South' }
+const WEST = {x: 0, y: -1, action: 'West' }
+const STAY = {x: 0, y: 0, action: 'Stay' }
+
+const DIRECTIONS = [NORTH, EAST, SOUTH, WEST]
 
 function bot(state, callback) {
-    var i = Math.floor(Math.random() * 4);
-    var dir = dirs[i];
-    callback(null, dir);
+    const game = new Game(state.game)
+    const hero = state.hero
+
+    const tiles = DIRECTIONS.map(direction => {
+        return {direction: direction, space: game.atPositionRelativeToHero(hero, direction)}
+    })
+    
+    let move = tiles.map(tile => {
+        switch (tile.space[0]) {
+            case TAVERN:
+                return tile.direction
+            case MINE:
+                if (parseInt(tile.space[1], 10) !== hero.id) {
+                    return tile.direction
+                }
+                break;
+            case AIR:
+                return tile.direction
+        }
+    }).find(direction => !!direction)
+
+    if (typeof move === 'undefined') {
+        move = STAY
+    }
+    
+    callback(null, move.action)
 };
 
-module.exports = bot;
+module.exports = bot
 if (require.main === module)
-    require('vindinium-client').cli(bot);
+    require('vindinium-client').cli(bot)
